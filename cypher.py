1ec9c6b1e92afa32051c57d6d942cb2edfaf0f22
MatThHeuss <matheusalencar99@gmail.com>
2023-04-30 16:25:37 -0300

    fix erros and add check key function

--- cypher.py
+++ cypher.py
@@ -1,3 +1,17 @@
+def checkKey(message: str, key: str):
+    sizeMessage = len(message)
+    sizeKey = len(key)
+
+    if (sizeKey >= sizeMessage):
+        return key
+    else:
+        aux = sizeMessage - sizeKey
+        for i in range(aux):
+            key+=key[i]
+
+        return key
+
+
 def encrypt(message, key):
     sizeMessage = len(message)
     j = 0
@@ -15,17 +29,18 @@ def encrypt(message, key):
     return cipher
 
 
-def decrypt(cypher: str, key: str):
-    sizeCypher = len(cypher)
-    j =0
+def decrypt(cipher: str, key: str) -> str:
+    sizeCipher = len(cipher)
+    j = 0
     message = ""
-    for i in range(sizeCypher):
-        if 'a' <= cypher[i] <= 'z':
-            letter = (ord(cypher[i]) - key[i -j] + ord('a')) % 26
+
+    for i in range(sizeCipher):
+        if 'a' <= cipher[i] <= 'z':
+            letter = (ord(cipher[i]) - ord(key[i-j]) + 26) % 26
             letter = chr(letter + ord('a'))
             message += letter
         else:
-            j+= 1
-            message += cypher[i]
-    
+            j += 1
+            message += cipher[i]
+
     return message
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-from cypher import encrypt, decrypt
+from cypher import encrypt, decrypt, checkKey
 
 
 def menu():
-    print("***********************")
-    print(" Escolha a opcao desejada:  ")
-    print("1 - Criptografar")
-    print("2 - Descriptografar")
-    option = int(input())
+    while True:
+        print("***********************")
+        print(" Escolha a opcao desejada:  ")
+        print("1 - Criptografar")
+        print("2 - Descriptografar")
+        print("3 - Ataque")
+        print("4 - Sair")
+        option = int(input())
 
-    if option == 1:
-        test = encrypt("test", "aaaa")
-        print(test)
-    elif option == 2:
-        test =  decrypt("test", "aaaa")
-        print(test)
-
+        if option == 1:
+            message = input("Entre com o valor da mensagem\n")
+            key = input("entre com o valor da chave:\n")
+            key = checkKey(message, key)
+            cifra =  encrypt(message, key)
+            print(f'Cifra = {cifra}')
+        elif option == 2:
+            cypher = input("Entre com o valor da cifra:\n")
+            key = input("Entre com o valor da chave:\n")
+            newkey = checkKey(cypher, key)
+            message =  decrypt(cypher, newkey)
+            print(f'Mensagem = {message}')
+        elif option == 3:
+            print("ataque")
+        else:
+            return False
 
 
 menu()
