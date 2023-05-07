a759229eab90c97905be32939e90a82343811bd3
Vinícius <31750046+vmsantos@users.noreply.github.com>
2023-05-06 21:56:52 -0300

    Add files via upload

--- attack.py
+++ attack.py
@@ -1,67 +1,78 @@
-def remove_character(get_cipher):
-    size_cipher = len(get_cipher)
+import math
+
+def removeCharacter(getCipher):
+    sizeCipher = 0
     cipher = ""
-    for i in range(size_cipher):
-        if 'a' <= get_cipher[i] <= 'z':
-            cipher += get_cipher[i]
+
+    sizeCipher = len(getCipher)
+    for i in range(sizeCipher):
+        if 'a' <= getCipher[i] <= 'z':
+            cipher += getCipher[i]
+
     return cipher
 
 
-def size_key(cipher):
+def sizeKey(cipher):
     size_cipher = 0
     size_frequency = 0
     s = 0
-    range_list = []
+    _range = []
     trigram = [0, 0, 0]
     frequency = [0] * 19
 
-    cipher = remove_character(cipher)
+    cipher = removeCharacter(cipher)
     size_cipher = len(cipher)
 
     for i in range(size_cipher - 3):
         trigram[0] = cipher[i]
-        trigram[1] = cipher[i+1]
-        trigram[2] = cipher[i+2]
+        trigram[1] = cipher[i + 1]
+        trigram[2] = cipher[i + 2]
         for k in range(i + 1, size_cipher - 2):
-            if trigram[0] == cipher[k] and trigram[1] == cipher[k+1] and trigram[2] == cipher[k+2]:
-                if (k-i) not in range_list:
-                    range_list.append(k-i)
+            if trigram[0] == cipher[k] and trigram[1] == cipher[k + 1] and trigram[2] == cipher[k + 2]:
+                if (k - i) not in _range:
+                    _range.append(k - i)
                 break
-    size_frequency = len(range_list)
-    for i in range(size_frequency):
-        for k in range(2, 21):
-            if range_list[i] % k == 0:
-                frequency[k-2] += 1
 
-    print("\n\n\nAnalisar possíveis tamanhos de chave por frequência e escolher um possível tamanho de chave:\n")
+    size_frequency = len(_range)
+    for i in range(size_frequency):
+        for k in range(2, 20):
+            if _range[i] % k == 0:
+                frequency[k - 2] += 1
+
+    print("\nAnalyze possible key sizes by frequency and choose a possible key size:\n")
     for i in range(10):
-        print("Possível tamanho de chave: {} = {}".format(i+2, frequency[i]))
-    print("\nInsira o tamanho da chave: ")
-    s = int(input())
+        print("Possible key size: ", i + 2, " = ", frequency[i])
+    s = int(input("\nEnter key size: "))
 
     return s
+
+
 
 def attack(cipher, sizeKey, language):
     sizeCipher = 0
     y = 0
     key = ""
-    
-    english = {'a':8.167,'b':1.492,'c':2.782,'d':4.253,'e':12.702,'f':2.228,
-    'g':2.015,'h':6.094,'i':6.966,'j':0.153,'k':0.772,'l':4.025,'m':2.406,'n':6.749,'o':7.507,
-    'p':1.929,'q':0.095,'r':5.987,'s':6.327,'t':9.056,'u':2.758,'v':0.978,'w':2.36,'x':0.15,
-    'y':1.974,'z':0.074}
+    english = {ord('a'): 8.167, ord('b'): 1.492, ord('c'): 2.782, ord('d'): 4.253, ord('e'): 12.702, 
+            ord('f'): 2.228, ord('g'): 2.015, ord('h'): 6.094, ord('i'): 6.966, ord('j'): 0.153, 
+            ord('k'): 0.772, ord('l'): 4.025, ord('m'): 2.406, ord('n'): 6.749, ord('o'): 7.507, 
+            ord('p'): 1.929, ord('q'): 0.095, ord('r'): 5.987, ord('s'): 6.327, ord('t'): 9.056, 
+            ord('u'): 2.758, ord('v'): 0.978, ord('w'): 2.36, ord('x'): 0.15, ord('y'): 1.974, 
+            ord('z'): 0.074}
 
-    portuguese = {'a':14.63,'b':1.04,'c':3.88,'d':4.99,'e':12.57,'f':1.02,
-    'g':1.30,'h':1.28,'i':6.18,'j':0.4,'k':0.02,'l':2.78,'m':4.74,'n':5.05,'o':10.73,
-    'p':2.52,'q':1.20,'r':6.53,'s':7.81,'t':4.34,'u':4.63,'v':1.67,'w':0.01,'x':0.21,
-    'y':0.01,'z':0.47}
+    portuguese = {ord('a'): 14.63, ord('b'): 1.04, ord('c'): 3.88, ord('d'): 4.99, ord('e'): 12.57, 
+                ord('f'): 1.02, ord('g'): 1.30, ord('h'): 1.28, ord('i'): 6.18, ord('j'): 0.4, 
+                ord('k'): 0.02, ord('l'): 2.78, ord('m'): 4.74, ord('n'): 5.05, ord('o'): 10.73, 
+                ord('p'): 2.52, ord('q'): 1.20, ord('r'): 6.53, ord('s'): 7.81, ord('t'): 4.34, 
+                ord('u'): 4.63, ord('v'): 1.67, ord('w'): 0.01, ord('x'): 0.21, ord('y'): 0.01, 
+                ord('z'): 0.47}
 
-    frequencyCipher = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,
-    'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,
-    'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,
-    'y':0,'z':0}
+    frequencyCipher = {ord('a'): 0, ord('b'): 0, ord('c'): 0, ord('d'): 0, ord('e'): 0, ord('f'): 0,
+                    ord('g'): 0, ord('h'): 0, ord('i'): 0, ord('j'): 0, ord('k'): 0, ord('l'): 0, 
+                    ord('m'): 0, ord('n'): 0, ord('o'): 0, ord('p'): 0, ord('q'): 0, ord('r'): 0, 
+                    ord('s'): 0, ord('t'): 0, ord('u'): 0, ord('v'): 0, ord('w'): 0, ord('x'): 0, 
+                    ord('y'): 0, ord('z'): 0}
 
-    cipher = remove_character(cipher)
+    cipher = removeCharacter(cipher)
     sizeCipher = len(cipher)
 
     y = sizeCipher // sizeKey
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cypher import encrypt, decrypt, checkKey
+from attack import sizeKey, attack
 
 
 def menu():
@@ -13,18 +14,27 @@ def menu():
 
         if option == 1:
             message = input("Entre com o valor da mensagem\n")
-            key = input("entre com o valor da chave:\n")
+            key = input("Entre com o valor da chave:\n")
             key = checkKey(message, key)
-            cifra =  encrypt(message, key)
+            cifra = encrypt(message, key)
             print(f'Cifra = {cifra}')
         elif option == 2:
             cypher = input("Entre com o valor da cifra:\n")
             key = input("Entre com o valor da chave:\n")
             newkey = checkKey(cypher, key)
-            message =  decrypt(cypher, newkey)
+            message = decrypt(cypher, newkey)
             print(f'Mensagem = {message}')
         elif option == 3:
-            print("ataque")
+            cipher = input("Entre com o valor da cifra:\n")
+            print("Idiomas disponíveis:\n")
+            print("1 - Inglês")
+            print("2 - Português")
+            language = int(input("Escolha um idioma:\n"))
+            key = attack(cipher, sizeKey(cipher), language)
+            key = checkKey(cipher, key)
+            message = decrypt(cipher, key)
+            print("\nMessage: " + message)
+            input("\nPRESS ENTER TO CONTINUE")
         else:
             return False
 
